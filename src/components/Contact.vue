ffe53d8d14152352ce91653d92db12dffc704ae0
or abdillh <hans.abdillh05@gmail.com>
2022-03-07 22:02:36 +0800

    Create handler for submit form to googlespreadsheets

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -21,40 +21,40 @@
 				</template>
 			</div>
 			
-			<form @submit.prevent="submitForm" class="lg:w-10/12 lg:mx-auto mt-8 md:flex items-start gap-5 justify-between">
+			<form ref="myForm" @submit.prevent="submitForm" class="lg:w-10/12 lg:mx-auto mt-8 md:flex items-start gap-5 justify-between">
 				<section class="md:w-6/12">
 					<InputField label="Fullname">
 						<template v-slot:input>
-							<input class="input" type="text" v-model="form.fullname" placeholder="your fullname" required/>
+							<input name="fullname" class="input" type="text" placeholder="your fullname" required/>
 						</template>
 					</InputField>
 					<InputField label="Company" icon="fa fa-building">
 						<template v-slot:input>
-							<input class="input" type="text" v-model="form.company" placeholder="your company name" required />
+							<input class="input" type="text" name="company" placeholder="your company name" required />
 						</template>
 					</InputField>
 					<InputField label="Phone" icon="fab fa-whatsapp">
 						<template v-slot:input>
-							<input class="input" type="number" v-model="form.phone" placeholder="your phone number" required />
+							<input class="input" type="number" name="phone" placeholder="your phone number" required />
 						</template>
 					</InputField>
 					<InputField label="Email" icon="fa fa-envelope">
 						<template v-slot:input>
-							<input class="input" type="email" v-model="form.email" placeholder="your email address" required />
+							<input class="input" type="email" name="email" placeholder="your email address" required />
 						</template>
 					</InputField>
 				</section>
 				
 				<section class="w-full md:w-5/12 flex flex-wrap">
 					<label class="text-base">Message</label>
-					<textarea rows="13" v-model="form.message" class="w-full text-base text-gray-600 mt-2 p-2 border-2 border-indigo-300 rounded-xl overflow-hidden bg-gray-50" placeholder="Your message"></textarea>
+					<textarea rows="13" name="message" class="w-full text-base text-gray-600 mt-2 p-2 border-2 border-indigo-300 rounded-xl overflow-hidden bg-gray-50" placeholder="Your message"></textarea>
 					<div class="w-full mt-5 flex justify-between">
 						<button :class="isLoading ? 'bg-blue-200' : 'bg-blue-500'" class="text-gray-100 w-5/12 py-2 rounded-lg text-base" type="submit">
-							<i class="fa fa-paper-plane mr-1"></i>
-							<span v-if="!isLoading">Send</span>
+							<i v-if="statusMsg !== 'Failed'" :class="statusMsg === 'Ok' ? 'fa-check' : 'fa-paper-plane'" class="fa mr-1"></i>
+							<span v-if="!isLoading">{{ statusMsg }}</span>
 							<span v-else>Sending ...</span>
 						</button>
-						<button class="border-red-500 border-2 text-red-600 w-5/12 py-2 rounded-lg text-base" type="reset">
+						<button @click="statusMsg = 'Send'" class="border-red-500 border-2 text-red-600 w-5/12 py-2 rounded-lg text-base" type="reset">
 							<i class="fa fa-times"></i>
 							Reset
 						</button>
@@ -70,37 +70,30 @@
 	import axios from 'axios'
 	import contacts from '@/contents/contacts.js'
 	import InputField from '@/components/InputField.vue'
-	import { reactive, ref } from 'vue'
+	import { ref } from 'vue'
 
-	const form = reactive({
-		fullname: '',
-		company: '',
-		phone: '',
-		email: '',
-		message: ''
-	})
+	const statusMsg = ref('Send')
+	const myForm = ref(null)
 	
 	const isLoading = ref(false)
-	const URL = 'https://script.google.com/macros/s/AKfycbxsfCd6Z5XDPXcC5gLkJL9hhwfiYYcArynlPIIOhFDh_7Yn5_AOYH_VeJ0EKdLtukuH/exec'
-
-	const headers = {
-		'content-type': 'application/json'
-	}
+	const URL = 'https://script.google.com/macros/s/AKfycbwDLoFe7LvF7-UkfCC6BNE68ORGJnkMdcCiwTVGOqlacDhh02R4abuOv9Rth-IqOrbcgQ/exec'
 	
 	const submitForm = () => {
 		isLoading.value = true
+		
 		fetch(URL, {
 			method: 'POST',
 			mode: 'no-cors',
-			headers,
-			body: form
+			body: new FormData(myForm.value)
 		})
-		.then( res => res.json() )
 		.then( res => {
-			console.log(res, 'Ok')
+			statusMsg.value = 'Ok'
 			isLoading.value = false
 		})
-		.catch( err => console.log(err, 'no'))
+		.catch( err => {
+			statusMsg.value = 'Failed'
+			isLoading.value = false
+		})
 	}
 
 </script>
